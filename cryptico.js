542f3c69bdc52bab2c07089b575f42738e155316
shibayan <shiba-yan@outlook.com>
2016-02-08 14:39:15 +0900

    Fixed AES key generator bug

--- cryptico.js
+++ cryptico.js
@@ -3382,7 +3382,7 @@ var cryptico = (function() {
     // AES CBC Encryption.
     my.encryptAESCBC = function(plaintext, key)
     {
-        var exkey = key.slice(0);
+        var exkey = my.string2bytes(key);
         aes.ExpandKey(exkey);
         var blocks = my.string2bytes(plaintext);
         blocks = my.pad16(blocks);
@@ -3402,7 +3402,7 @@ var cryptico = (function() {
     // AES CBC Decryption.
     my.decryptAESCBC = function(encryptedText, key)
     {
-        var exkey = key.slice(0);
+        var exkey = my.string2bytes(key);
         aes.ExpandKey(exkey);
         var encryptedText = my.b64to256(encryptedText);
         var encryptedBlocks = my.string2bytes(encryptedText);
